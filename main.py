142429e53f5a5b091e4a444f48ade06b8cbe8ef2
kramstyles <kramstyles@outlook.com>
2022-02-21 20:14:49 +0100

    working on check_transactions

--- main.py
+++ main.py
@@ -1,35 +1,54 @@
 # The entry point of your application
-from cmath import log
-from pip import main
 from data.data import FORMAT as format, resources
 from assets.art import logo
+
 
 class Printer():
     # off turns off the system
     # report prints out current system status
 
+    resource = resources
     currency = {
-        'Penny':0.01,
-        'Nickel':0.05,
-        'Dime':0.10,
-        'Quarter':0.25
+        'Penny': 0.01,
+        'Nickel': 0.05,
+        'Dime': 0.10,
+        'Quarter': 0.25
     }
-
+    prices = {'coloured': 35, 'grayscale': 25}
 
     def __init__(self):
+        self.type = input('What format would you like? ( coloured or grayscale )')
         self.pages = input('Enter Number of Pages to Print! ')
         self.type = input('Enter Cointype', 'Hey: ')
         self.coins = input('Number of Coins: ')
 
+    def check_resources(self):
+        pass
+
+    def process_price(self, pages):
+        self.total = pages * Printer.prices[self.type]
+        user_coins = input(f'Your Transaction would cost ${self.total}. Enter Amount you would pay')
+        return user_coins
+
+    def check_transactions(self, ):
+        user_coins = self.process_price(self.pages)
+        if self.total > user_coins:
+            
+    @classmethod
     def turn_off(self):
         print("Bye!")
         SystemExit()
 
-    def checkTransactions(self):
-        total = self.pages * self.coins
+    @classmethod
+    def report(self):
+        print(f"Paper: {resources['paper']}pc")
+        print(f"Ink: {resources['ink']}ml")
+        print(f"Profit: ${resources['profit']}")
+
 
 class GreyPrinter(Printer):
     pass
+
 
 class ColoredPrinter(Printer):
     pass
@@ -40,6 +59,6 @@ class ColoredPrinter(Printer):
 # job2 = ColoredPrinter()
 # job1.turn_off()
 
+
 if __name__ == '__main__':
-    print(logo)
-    job1 = GreyPrinter()
+    Printer.process_price('', 12)
