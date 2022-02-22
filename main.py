a9e13695b1ad03888d3214006e21aba88fc73119
kramstyles <kramstyles@outlook.com>
2022-02-22 07:29:59 +0100

    accepted coins from process_price

--- main.py
+++ main.py
@@ -8,19 +8,12 @@ class Printer():
     # report prints out current system status
 
     resource = resources
-    currency = {
-        'Penny': 0.01,
-        'Nickel': 0.05,
-        'Dime': 0.10,
-        'Quarter': 0.25
-    }
+    currency = {'Pennies': 0.01, 'Nickels': 0.05, 'Dimes': 0.10, 'Quarters': 0.25}
 
     def __init__(self):
         print(logo)
         self.print_type = input('What format would you like? ( coloured or greyscale ): ')
         self.pages = int(input('Enter Number of Pages to Print! '))
-        # self.coin_type = input('Enter Cointype', 'Hey: ')
-        # self.coins = input('Number of Coins: ')
         self.process_price()
 
 
@@ -38,21 +31,25 @@ class Printer():
         if self.check_resources():
             price = print_format[self.print_type]['price']
             self.total = self.pages * price
-            user_coins = input(f'Your Transaction would cost ${self.total}. Enter Amount you would pay: ')
-            print(user_coins)
+            print(f'Your Transaction would cost ${self.total}. ')
+            user_coins = 0
+            for currency in Printer.currency:
+                user_coins += int(input(f"Enter amount you want to pay in {currency}: ")) * Printer.currency[currency]
             return user_coins
 
     def check_transactions(self):
-        user_coins = self.process_price(self.pages)
-        if self.total > user_coins:
-            return "Sorry that’s not enough coins. Coins refunded"
-        else:
-            Printer.resource['ink'] -= (print_format[self.print_type]['materials']['ink'] * self.pages)
-            Printer.resource['paper'] -= self.pages
-            Printer.resource['profit'] += self.total
-            if user_coins > self.total:
-                print('This is your change:', user_coins - self.total)
-            return True
+        if self.process_price:
+            user_coins = self.process_price()
+            if self.total > user_coins:
+                print ("Sorry that’s not enough coins. Coins refunded")
+            else:
+                self.report()
+                Printer.resource['ink'] -= (print_format[self.print_type]['materials']['ink'] * self.pages)
+                Printer.resource['paper'] -= self.pages
+                Printer.resource['profit'] += self.total
+                if user_coins > self.total:
+                    print('This is your change:', user_coins - self.total)
+                return True
     
     def thank_you(self):
         print("Here's your project")
